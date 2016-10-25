9bd718699c69f46e93c1eb7029966a5a3db03f4f
Claudia <zavalacl@msu.edu>
2016-10-25 15:54:56 -0400

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a = 63;
+		int b = 45;
+		
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		
+		a = a + 10;
+		a += 10; //extended assignment operator
+		
+		a = a + 1;
+		a += 1;
+		a++; //increment
+
+		System.out.println("a is " + a);
+		
+		double c = 3.5;
+		int d = (int) c;
+		long e = Math.round(c);
+		System.out.println("c is " + c);
+		System.out.println("d is " + d);
+		System.out.println("e is " + e);
+
+
+
+	}
+
+}
